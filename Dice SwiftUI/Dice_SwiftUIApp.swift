b3d1440b78bb5834832521296cc6fc12e19723f8
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-09 20:01:01 -0700

    Initial Commit

---
+++ Dice SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice SwiftUI
+//
+//  Created by Nastya Klyashtorna on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
